6b0dd5d9cbbca07ad7e32c62d9a8718cc22543b5
Fateev Dmitriy <fateev@inbox.ru>
2025-01-12 22:29:26 +0300

    Init

---
+++ src/main/java/ru/otus/java/basic/lesson1/Application.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.lesson1;
+
+public class Application {
+    // my first app entry point
+    public static void main(String[] args) {
+        System.out.println("Hello World One!");
+        System.out.println("Hello World Two!");
+        System.out.println("Hello World Three!");
+    }
+}
---
+++ src/ru/otus/java/basic/lesson1/Application.java
@@ -0,0 +1,5 @@
+package ru.otus.java.basic.lesson1;
+
+public class Application {
+
+}
